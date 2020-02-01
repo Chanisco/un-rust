456cbf55b055740d66c36bb63258865bdd4eca82
DESKTOP-I3RJQLK\Aaron <aaronligthart@hotmail.com>
2020-02-01 13:45:32 +0100

    added button

---
+++ Assets/MachineButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineButton : MonoBehaviour
+{
+    public Collider buttonCollider;
+    public Material buttonMaterial;
+    public Color buttonColor;
+    public enum buttonSize{big, medium, small, tiny };
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonMaterial.color = buttonColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
